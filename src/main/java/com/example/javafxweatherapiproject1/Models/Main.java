95e2ba3acc40f085514e007e786b3917b8379fba
Chisato <49408516+choco999@users.noreply.github.com>
2021-12-06 16:01:56 -0500

    Changed variable name for the naming convention

--- src/main/java/com/example/javafxweatherapiproject1/Models/Main.java
+++ src/main/java/com/example/javafxweatherapiproject1/Models/Main.java
@@ -1,18 +1,27 @@
 package com.example.javafxweatherapiproject1.Models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Main {
     private float temp;
-    private float feels_like;
-    private float temp_min;
-    private float temp_max;
+
+    @SerializedName("feels_like")
+    private float feelsLike;
+
+    @SerializedName("temp_min")
+    private float tempMin;
+
+    @SerializedName("temp_max")
+    private float tempMax;
+
     private int pressure;
     private int humidity;
 
-    public Main(float temp, float feels_like, float temp_min, float temp_max, int pressure, int humidity) {
+    public Main(float temp, float feelsLike, float tempMin, float tempMax, int pressure, int humidity) {
         this.temp = temp;
-        this.feels_like = feels_like;
-        this.temp_min = temp_min;
-        this.temp_max = temp_max;
+        this.feelsLike = feelsLike;
+        this.tempMin = tempMin;
+        this.tempMax = tempMax;
         this.pressure = pressure;
         this.humidity = humidity;
     }
@@ -25,28 +34,28 @@ public class Main {
         this.temp = temp;
     }
 
-    public float getFeels_like() {
-        return feels_like;
+    public float getFeelsLike() {
+        return feelsLike;
     }
 
-    public void setFeels_like(float feels_like) {
-        this.feels_like = feels_like;
+    public void setFeelsLike(float feelsLike) {
+        this.feelsLike = feelsLike;
     }
 
-    public float getTemp_min() {
-        return temp_min;
+    public float getTempMin() {
+        return tempMin;
     }
 
-    public void setTemp_min(float temp_min) {
-        this.temp_min = temp_min;
+    public void setTempMin(float tempMin) {
+        this.tempMin = tempMin;
     }
 
-    public float getTemp_max() {
-        return temp_max;
+    public float getTempMax() {
+        return tempMax;
     }
 
-    public void setTemp_max(float temp_max) {
-        this.temp_max = temp_max;
+    public void setTempMax(float tempMax) {
+        this.tempMax = tempMax;
     }
 
     public int getPressure() {
